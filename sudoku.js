43b155c2558e832991df9b728eb6d23a42f4b49f
Kerim <charim.nio@yandex.ru>
2023-01-20 14:00:47 +0300

    получили 9 горизонтов в каждом уровне

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,33 @@
+const fs = require("fs");
+const { EOL } = require("os");
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {}
+
+const boardString = fs.readFileSync(`${__dirname}/puzzles.txt`, "utf-8");
+
+function solve(boardString) {
+  const re = /\S{9}/g;
+  return boardString
+    .split(EOL)
+    .map((el) => [el])
+    .map((el) => el.join().match(re));
+}
+
+console.log(solve(boardString));
+
+// console.log(solve(boardString));
+
+// const a = solve(boardString);
+
+// const re = /\S{9}/g;
+// const reGorizont = a[0].join().match(re);
+
+// console.log(reGorizont);
+
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
