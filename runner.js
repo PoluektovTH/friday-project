2b09a6b643e719be7dae5adc0b4c5ddeb8e68ff3
Igor <igorpoluektov74@gmail.com>
2023-01-21 20:06:32 +0300

    finalStyle

--- runner.js
+++ runner.js
@@ -7,4 +7,3 @@ const readAndSolve = require('./readAndSolve');
 // Все судоку для решения доступны в файле puzzles.txt.
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
 fs.readFile('./puzzles.txt', 'utf-8', readAndSolve);
-// qewfwefesfs;
